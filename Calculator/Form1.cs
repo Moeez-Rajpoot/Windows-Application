97872bdf1ac577574e022134c8088c9cf7606012
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2022-10-02 21:50:26 +0500

    Updated
    Basic Operations Application

---
+++ Calculator/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                label4.Text = (a + b).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("invalid input");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                label4.Text = (a - b).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("invalid input");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                label4.Text = (a * b).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("invalid input");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                label4.Text = (a / b).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("invalid input");
+            }
+        }
+    }
+}
